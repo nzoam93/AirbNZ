ffb4785a562686e8c14a83de230beb391da2af48
Noam Z <nzoam93@gmail.com>
2023-01-07 10:46:47 -0800

    fixed profile dropdown. added global colors

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -40,21 +40,18 @@ function ProfileButton({ user }) {
   return (
     <div className='login-links'>
       {/* !showmenu in order to make it not show up when the menu is open */}
-      {!showMenu && <button onClick={openMenu} className='icon'>
-        {/* <i className="fa-solid fa-user-circle fa-2x" /> */}
-        <div id="welcome-corner">
-          <div>Welcome, {user.username}</div>
+      {/* {!showMenu && <button onClick={openMenu} className='icon'>
+        <i className="fa-solid fa-user-circle fa-2x" />
+      </button>} */}
+      {!showMenu && (
+        <>
           <div id="profile-img">{profileImg}</div>
-        </div>
-      </button>}
-      {showMenu && (
-        <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
+          <ul id="profile-dropdown">
+            <li>{user.username}</li>
+            <button id="logout-button" onClick={logout}>Log Out</button>
+          </ul>
+        </>
+
       )}
     </div>
   );
