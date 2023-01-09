cf6cfb857fe3943de5c819efe78ff85587b1c102
Noam Z <nzoam93@gmail.com>
2023-01-09 09:20:39 -0800

    added progress bars for ratings

--- frontend/src/components/Reviews/ReviewIndex.jsx
+++ frontend/src/components/Reviews/ReviewIndex.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchReviews, getReviews } from "../../store/reviews";
 import ReviewIndexItem from "./ReviewIndexItem";
 import './Review.css';
+import './ReviewIndex.css'
 import { useParams } from "react-router-dom";
 import ReviewForm from "./ReviewForm";
 
@@ -35,7 +36,6 @@ const ReviewIndex = () => {
 
     return(
         <>
-
             <div className="title-show">
                 <i className="fa-solid fa-star show-page-icon" />
                 {numReviews == 0 ? 5 : avgRating} •
@@ -43,6 +43,53 @@ const ReviewIndex = () => {
                 {numReviews}
                 {numReviews == 1 ? ' Review' : ' Reviews'}
             </div>
+
+            <div id="progress-container">
+                <div className="progress-item">
+                    <label for="cleanliness">Cleanliness</label>
+                    <div id="progress-rating">
+                        <progress id="cleanliness" value="3" max="5">3</progress>
+                        <p>3</p>
+                    </div>
+                </div>
+                <div className="progress-item">
+                    <label for="cleanliness">Communication</label>
+                    <div id="progress-rating">
+                        <progress id="cleanliness" value="3" max="5">3</progress>
+                        <p>3</p>
+                    </div>
+                </div>
+                <div className="progress-item">
+                    <label for="cleanliness">Check-in</label>
+                    <div id="progress-rating">
+                        <progress id="cleanliness" value="3" max="5">3</progress>
+                        <p>3</p>
+                    </div>
+                </div>
+                <div className="progress-item">
+                    <label for="cleanliness">Accuracy</label>
+                    <div id="progress-rating">
+                        <progress id="cleanliness" value="3" max="5">3</progress>
+                        <p>3</p>
+                    </div>
+                </div>
+                <div className="progress-item">
+                    <label for="cleanliness">Location</label>
+                    <div id="progress-rating">
+                        <progress id="cleanliness" value="3" max="5">3</progress>
+                        <p>3</p>
+                    </div>
+                </div>
+                <div className="progress-item">
+                    <label for="cleanliness">Value</label>
+                    <div id="progress-rating">
+                        <progress id="cleanliness" value="3" max="5">3</progress>
+                        <p>3</p>
+                    </div>
+                </div>
+            </div>
+
+
             <div id="write-a-review-button" onClick={toggleReviewForm}>Write a review</div>
 
             {/* only appear if user clicks the toggleReviewForm button above */}
