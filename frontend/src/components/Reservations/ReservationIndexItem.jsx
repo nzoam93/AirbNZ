aecc06f51f3f68227040276ec1ae94e1a7209fc3
Noam Z <nzoam93@gmail.com>
2023-01-10 15:43:32 -0800

    staging

--- frontend/src/components/Reservations/ReservationIndexItem.jsx
+++ frontend/src/components/Reservations/ReservationIndexItem.jsx
@@ -21,28 +21,30 @@ const ReservationIndexItem = ({reservation}) => {
     }
 
     return(
-        <div id="reservation-index-item">
-            <Link to={`/listings/${reservation.listingId}`}>
-                {/* In order to use the below line, I need to get the listing.photoUrls in the reservation state
-                OR ALTERNATIVELY, just do a listing = useSelector(state.session.listing) */}
-                {/* <div id="house-img"><img src={listing.photoUrls[0]} alt="house" /></div> */}
-                <div id="reservation-house-img">{houseImg}</div>
-            </Link>
-            <div id="reservation-container">
-                <p id="reservation-title">{reservation.listingTitle}</p>
-                <div id="reservation-info">
-                    {reservation.startDate} to {reservation.endDate} • {reservation.numGuests} guests
+        <div id="reservation-index-item-container">
+            <div id="reservation-index-item">
+                <Link to={`/listings/${reservation.listingId}`}>
+                    {/* In order to use the below line, I need to get the listing.photoUrls in the reservation state
+                    OR ALTERNATIVELY, just do a listing = useSelector(state.session.listing) */}
+                    {/* <div id="house-img"><img src={listing.photoUrls[0]} alt="house" /></div> */}
+                    <div id="reservation-house-img">{houseImg}</div>
+                </Link>
+                <div id="reservation-container">
+                    <p id="reservation-title">{reservation.listingTitle}</p>
+                    <div id="reservation-info">
+                        {reservation.startDate} to {reservation.endDate} • {reservation.numGuests} guests
+                    </div>
+                    <div id="show-page-buttons">
+                        <p onClick={toggleReservationForm} className="show-page-button">Update </p>
+                        <p onClick={handleDelete} className="show-page-button"> Cancel</p>
+                    </div>
                 </div>
-                <div id="show-page-buttons">
-                    <p onClick={toggleReservationForm} className="show-page-button">Update </p>
-                    <p onClick={handleDelete} className="show-page-button"> Cancel</p>
-                </div>
+                {showReservationFormEdit ?
+                    <div id="reservation-form-modal">
+                        <ReservationFormEdit reservation = {reservation} setShowReservationFormEdit = {setShowReservationFormEdit}/>
+                    </div>
+                : ""}
             </div>
-            {showReservationFormEdit ?
-                <div id="reservation-form-modal">
-                    <ReservationFormEdit reservation = {reservation} setShowReservationFormEdit = {setShowReservationFormEdit}/>
-                </div>
-            : ""}
         </div>
     )
 }
